cda58abb00a1b0b9642c0a15cb3364d50e4b9d4c
gzippedfrog <gzippedfrog@outlook.com>
2023-05-22 12:47:00 +0300

    updated error page

--- src/views/error.view.php
+++ src/views/error.view.php
@@ -1,18 +1,19 @@
 <?php view('partials/head') ?>
 
-<div class="text-center flex-1 flex flex-col justify-center items-center">
-    <h1 class="mb-16 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Error <span class="text-blue-700"><?= $code ?></span>: <?= $message ?></h1>
-    <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-
-    </p>
-    <a href="/" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-        <div>
-            Go to the main page
+<section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-sm text-center">
+            <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-gray-900 dark:text-blue-500">
+                <?= $code ?>
+            </h1>
+            <p class="mb-4 text-3xl tracking-tight font-bold text-gray-500 md:text-4xl dark:text-white">
+                <?= $message ?>
+            </p>
+            <a href="/"
+                class="inline-flex text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-blue-900 my-4">Back
+                to Homepage</a>
         </div>
-        <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-        </svg>
-    </a>
-</div>
+    </div>
+</section>
 
 <?php view('partials/footer') ?>
