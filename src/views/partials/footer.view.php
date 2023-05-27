01df99a7fdd338dfd96170e8363f3375a3c56193
gzippedfrog <gzippedfrog@outlook.com>
2023-05-27 14:11:02 +0300

    added modal for new posts

--- src/views/partials/footer.view.php
+++ src/views/partials/footer.view.php
@@ -1,4 +1,5 @@
 </main>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 </body>
 
 </html>
--- src/views/partials/head.view.php
+++ src/views/partials/head.view.php
@@ -10,7 +10,6 @@
     </title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 </head>
 
 <body class="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900">
