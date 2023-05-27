a7d872ca8ba46d5680c562186c5e2333a812b9f5
gzippedfrog <gzippedfrog@outlook.com>
2023-05-27 16:03:31 +0300

    fixed error page background

--- src/views/error.view.php
+++ src/views/error.view.php
@@ -1,6 +1,6 @@
 <?php view('partials/head') ?>
 
-<section class="bg-white dark:bg-gray-900">
+<section>
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div class="mx-auto max-w-screen-sm text-center">
             <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-gray-900 dark:text-blue-500">
