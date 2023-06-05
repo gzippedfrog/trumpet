29063fa2378e756d88fa177d8541444d7931124a
gzippedfrog <gzippedfrog@outlook.com>
2023-06-05 20:11:28 +0300

    updated styles

--- src/views/partials/head.view.php
+++ src/views/partials/head.view.php
@@ -12,16 +12,16 @@
 </head>
 
 <body class="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900">
-    <nav class="w-full bg-gray-800 text-white dark:bg-gray-900 dark:text-white sticky top-0 shadow-sm">
+    <nav class="w-full bg-gray-800 text-white dark:bg-gray-900 sticky top-0 shadow-sm">
         <div class="max-w-screen-xl mx-auto flex flex-wrap items-center justify-between p-4">
             <a href="/" class="self-center text-2xl font-semibold whitespace-nowrap">Trumpet</a>
 
             <?php if (isset($_SESSION['username'])): ?>
                 <div>
-                    Hi, <span class="text-primary-500 dark:text-primary-600 mr-6">
+                    Hi, <span class="text-primary-500 dark:text-primary-400 mr-6">
                         <?= "@" . htmlspecialchars($_SESSION['username']) ?>
                     </span>
-                    <form class="inline hover:text-primary-700 dark:hover:text-primary-600 cursor-pointer" method="POST"
+                    <form class="inline hover:text-primary-500 dark:hover:text-primary-400 cursor-pointer" method="POST"
                         action="/session">
                         <input type="hidden" name="_method" value="DELETE">
                         <button type="submit">Log out</button>
--- src/views/partials/post/edit_form.view.php
+++ src/views/partials/post/edit_form.view.php
@@ -15,11 +15,13 @@
             </div>
         <?php endif; ?>
 
-        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="image_file_input">Upload
-            image (optional)</label>
-        <input
-            class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-            id="image_file_input" name="image" type="file">
+        <div>
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="image_file_input">Upload
+                image (optional)</label>
+            <input
+                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                id="image_file_input" name="image" type="file">
+        </div>
 
         <button type="submit"
             class="w-min text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Submit</button>
