8479729cdf814bde3b70ffed0e5a2ebc7441f163
gzippedfrog <gzippedfrog@outlook.com>
2023-07-03 18:18:49 +0300

    removed redundant Database class

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,7 +5,6 @@ require base_path("../vendor/autoload.php");
 
 use Core\App;
 use Core\Container;
-use Core\Database;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
@@ -14,15 +13,7 @@ session_start();
 
 $container = new Container();
 
-$container->bind(Database::class, function () {
-    $config = parse_ini_file('../config.ini');
-
-    return new Database($config);
-});
-
-
 $container->bind(EntityManager::class, function () {
-
     $paths = [BASE_PATH];
     $isDevMode = true;
 
